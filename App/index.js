bdd64d2e9a95129d0b4f167280f90ae4d2c71d42
marco antonio bohorquez <antoniob@overskull.pe>
2021-03-17 14:00:45 -0500

    factorizar

--- App/index.js
+++ App/index.js
@@ -32,14 +32,17 @@ const scrapingProfile = ()=>{
     window.scrollTo(0,document.body.scrollHeight);
     wait(2000).then(function(){
 
-        const elementNameProfile = document.querySelector("div.ph5.pb5 > div.display-flex.mt2 ul li")
-        data.personal.name= elementNameProfile? elementNameProfile.innerText:'';
+    
+        const dataInline = function(element,string){
+            const selector = element.querySelector(string)
+            return  selector? selector.innerText:'';
+        };
 
-        const elementNameTitle = document.querySelector("div.ph5.pb5 > div.display-flex.mt2 h2")
-        data.personal.title= elementNameTitle? elementNameTitle.innerText:'';
+        const element = document.querySelector("div.ph5.pb5 > div.display-flex.mt2 ")
+        data.personal.name = dataInline(element,"ul li")
+        data.personal.title = dataInline(element,"h2")
+        data.personal.location = dataInline(element,"ul.pv-top-card--list-bullet li")
 
-         const elementNameUbicacion = document.querySelector("div.ph5.pb5 > div.display-flex.mt2 ul.pv-top-card--list-bullet li")
-        data.personal.location= elementNameUbicacion? elementNameUbicacion.innerText:'';
 
         wait(2000)
         const elementMoreResume = document.getElementById('line-clamp-show-more-button')
@@ -47,17 +50,17 @@ const scrapingProfile = ()=>{
         const elementResume = document.querySelector('section.pv-about-section > p')
         data.personal.aboutme= elementResume.innerText
 
-        const education = document.querySelector('#education-section').getElementsByTagName('li');
 
+        const education = document.querySelector('#education-section').getElementsByTagName('li');
         for(let i=0; i<education.length; i++){
-             const company = education[i].querySelector('a > .pv-entity__summary-info  h3').innerText;
-             const level = education[i].querySelector('a > .pv-entity__summary-info  p span.pv-entity__comma-item').innerText;
+             const element =  education[i].querySelector('a > .pv-entity__summary-info ');
+             const company = dataInline(element,'h3');
+             const level = dataInline(element,'p span.pv-entity__comma-item');
              const period  = education[i].querySelectorAll('div p.pv-entity__dates span')[1]?.innerText||"";
              data.education.push({company,level,period})
         }
 
         const experience = document.querySelectorAll('#experience-section ul.pv-profile-section__section-info > li');
-
         for(let i=0; i<experience.length; i++){
              let company = experience[i].querySelectorAll('a > .pv-entity__summary-info p')[1]?.innerText||"";
              if(company.length == 0){
@@ -71,7 +74,6 @@ const scrapingProfile = ()=>{
                     const functions = (selector != null)?selector.innerText:'';
                     puestos_all.push({cargo,period,functions});
                 }
-
                 data.experience.push({company,puestos_all});
              }else{
                 const period = experience[i].querySelectorAll('a > .pv-entity__summary-info h4 span')[1]?.innerText||"";  
